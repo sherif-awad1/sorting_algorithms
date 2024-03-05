1ac6034ecff2e7651d39ac08f357266eee4eb05b
Sherif Awad <sherif.hero1@gmail.com>
2024-03-06 01:14:32 +0200

    first test

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+/*
+File: 0-bubble_sort.c
+Author: Sherif Awad
+*/
+#include "sort.h"
+
+/**
+ * bubble_sort - sort array from low to high
+ * @array: array
+ * @size: array size
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+	size_t i, j, Sort = 0;
+
+	if (size < 2)
+		return;
+	for (i = 0; i < size; i++)
+		for (j = 0; j < size; j++)
+		{
+			if (array[j] > array[j + 1] && array[j + 1])
+			{
+			Sort = array[j];
+			array[j] = array[j + 1];
+			array[j + 1] = Sort;
+			print_array(array, size);
+			}
+		}
+}
+
